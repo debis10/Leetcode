0aad97cae15791ac904d2416deb9a8b18040dbce
Debis <debismandal4234@gmail.com>
2025-09-12 16:46:34 +0530

    Add files via upload

---
+++ IsSubsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int count=0;
+        int j=0;
+        for(int i=0; i<t.size(); i++)
+        {
+            if(s[j]== t[i])
+            {
+                count++;
+                j++;
+            }
+        }
+        if(count==s.size())
+            return true;
+        else
+            return false;
+    }
+};
